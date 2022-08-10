de80a92b8e523d2c74877e9d03b20a4d51691242
Amir Arad <greenshade@gmail.com>
2022-08-10 10:58:07 +0300

    gracefull ship node cleanup

--- src/ship-in/ship-in.ts
+++ src/ship-in/ship-in.ts
@@ -1,5 +1,7 @@
 import { Node, NodeDef, NodeInitializer, NodeMessage } from 'node-red';
 
+import { Destructors } from '@starwards/core';
+import { Event } from 'colyseus-events';
 import { StarwardsConfigNode } from '../starwards-config/starwards-config';
 
 export interface ShipInOptions {
@@ -9,33 +11,39 @@ export interface ShipInOptions {
 }
 export interface ShipInNode extends Node {
     configNode: StarwardsConfigNode;
+    destructors: Destructors;
+}
+
+async function nodeLogic(node: ShipInNode, { pattern, shipId }: ShipInOptions) {
+    try {
+        const shipDriver = await node.configNode.driver.getShipDriver(shipId);
+        node.status({});
+
+        const handleStateEvent = (e: Event) => {
+            node.send({ topic: e.path, payload: e.op === 'remove' ? undefined : e.value } as NodeMessage);
+        };
+        node.destructors.add(() => shipDriver.events.off(pattern, handleStateEvent));
+        shipDriver.events.on(pattern, handleStateEvent);
+    } catch (e) {
+        if (e instanceof Error) {
+            node.status({ fill: 'red', shape: 'dot', text: 'err:' + e.message });
+        } else {
+            node.status({ fill: 'red', shape: 'dot', text: JSON.stringify(e) });
+        }
+    }
 }
 
 const nodeInit: NodeInitializer = (RED): void => {
     function ShipInNodeConstructor(this: ShipInNode, config: NodeDef & ShipInOptions): void {
         RED.nodes.createNode(this, config);
+        this.destructors = new Destructors();
+        this.on('close', () => this.destructors.destroy());
         const configNode = RED.nodes.getNode(config.configNode) as StarwardsConfigNode | undefined;
         if (configNode) {
             this.configNode = configNode;
-            void (async () => {
-                try {
-                    const shipDriver = await this.configNode.driver.getShipDriver(config.shipId);
-                    shipDriver.events.on(config.pattern, (e) => {
-                        this.send({ topic: e.path, payload: e.op === 'remove' ? undefined : e.value } as NodeMessage);
-                    });
-                } catch (e) {
-                    if (e instanceof Error) {
-                        // eslint-disable-next-line no-console
-                        console.error(`error`, e, e?.stack);
-                    } else {
-                        // eslint-disable-next-line no-console
-                        console.error(`error`, e);
-                    }
-                }
-            })();
+            void nodeLogic(this, config);
         } else {
-            // eslint-disable-next-line no-console
-            console.error(`No config node configured`);
+            this.status({ fill: 'red', shape: 'ring', text: 'Server config missing or inactive' });
         }
     }
 
